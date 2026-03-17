e61f3b6567bd091e3407b461a0682b36c8b6f8a6
Paul Akintade <tadepaul14@gmail.com>
2026-03-17 19:20:23 +0000

    File changes

--- src/pages/VeloTrack.jsx
+++ src/pages/VeloTrack.jsx
@@ -162,6 +162,7 @@ export default function VeloTrack() {
                 trackedPoints={trackedPoints}
                 isTracking={isTracking}
                 onCanvasClick={handleCanvasClick}
+                onAutoTrackPoint={handleAutoTrackPoint}
               />
             ) : (
               <div className="flex flex-col items-center justify-center gap-4 text-center max-w-sm">
